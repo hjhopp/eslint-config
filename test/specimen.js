fff872f4fd811c60661416a090115d6f1b1918ae
kevinkace <kevinacameron@hotmail.com>
2018-08-22 13:03:16 -0700

    Update eslint version

--- test/specimen.js
+++ test/specimen.js
@@ -1,12 +1,15 @@
 "use strict";
 
+const thing = "test";
+const thing2 = "test";
+
 (function() {
     var fn = () => {},
         tagged   = fn`bar`,
         // template literal
         template = `foo ${tagged}`,
         boolean = Boolean(template),
-        
+
         obj = {
             noquote : true,
             "yes-quote" : true
